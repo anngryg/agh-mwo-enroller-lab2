253a07cc96ac70815b9e6b3d752749aa4c8ef2b6
Anna Grygierczyk <anna.grygierczyk@wp.pl>
2024-05-21 18:18:15 +0200

    authorization finished

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,18 +1,53 @@
 package com.company.enroller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService participantProvider;
+
+    @Value("${security.secret}")
+    String secret;
+
+    @Value("${security.issuer}")
+    String issuer;
+
+    @Value("${security.tokenExpiration}")
+    int tokenExpiration;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder1());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder1() {
+        return new BCryptPasswordEncoder();
     }
 }
